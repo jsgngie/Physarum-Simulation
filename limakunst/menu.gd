0b006759ae86b1a1e56aa625fac3015d460ac374
Joosep Suuder <suuderjoosep@gmail.com>
2024-11-11 03:13:02 +0200

    shader start
    Added basic shader, that draws n number of actors on the screen and moves them according to random velocity.

--- limakunst/menu.gd
+++ limakunst/menu.gd
@@ -3,11 +3,9 @@ extends Control
 @onready var displayed_number_of_actors_group = $MarginContainer/VBoxContainer/InteractableContainer/Sliders/SliderLabelsDisplayed/DisplayedNumberOfActorsGroup
 @onready var displayed_number_of_groups = $MarginContainer/VBoxContainer/InteractableContainer/Sliders/SliderLabelsDisplayed/DisplayedNumberOfGroups
 
-
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://slimulation.tscn")
 	
-
 func _on_quit_button_pressed():
 	get_tree().quit()
 
@@ -17,7 +15,6 @@ func _on_slider_actors_in_groups_value_changed(value):
 
 func _on_slider_groups_value_changed(value):
 	displayed_number_of_groups.text = String.num(value, 0)
-
 
 func _on_ready():
 	pass
