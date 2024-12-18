2c7d3d861245b3f785886f4554b6626ba2dd4acb
HenriM <henrimaandi@gmail.com>
2024-12-18 14:14:38 +0200

    profiling and optimizations

--- limakunst/slimeGPU.gd
+++ limakunst/slimeGPU.gd
@@ -36,18 +36,21 @@ var pipeline : RID
 var bindings : Array = []
 var dispatchSize : int
 
+var tempImg : Image
+
 @export var radius = 10
 @export var simulate : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	numActors = Global.number_of_actors_in_groups
-	actorSpeed = Global.speed_of_actors
+	#numActors = Global.number_of_actors_in_groups
+	#actorSpeed = Global.speed_of_actors
 	
 	dispatchSize = ceili(float(numActors) / float(work_group_size))
 	mat = $SubViewportContainer/SubViewport/TextureRect.material
 	img = Image.create_empty(width, height, false, Image.FORMAT_RGBA8)
 	tex = ImageTexture.create_from_image(img)
+	tempImg = Image.create_empty(width, height, false, Image.FORMAT_RGBA8)
 	for i in range(numActors):
 		var rot = randf_range(0.0, 2.0*PI)
 		randomize()
@@ -131,18 +134,30 @@ func _ready() -> void:
 	uniform_set = rd.uniform_set_create(bindings, shader, 0)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
+	var start = Time.get_ticks_msec()
 	viewPortTex = $SubViewportContainer/SubViewport.get_texture()
+	print("getting texture: " + str(Time.get_ticks_msec() - start))
+	start = Time.get_ticks_msec()
 	viewPortImg = viewPortTex.get_image()
+	print("getting image: " + str(Time.get_ticks_msec() - start))
 	#viewPortImg.resize(width, height, 0)
 	#print(viewPortImg.get_format())
 	#viewPortImg.convert(Image.FORMAT_RF)
+	start = Time.get_ticks_msec()
 	_process_compute()
+	print("process compute: " + str(Time.get_ticks_msec() - start))
+	start = Time.get_ticks_msec()
 	var trailMapData := rd.texture_get_data(trailMapOutBuffer, 0)
+	print("getting new data: " + str(Time.get_ticks_msec() - start))
+	start = Time.get_ticks_msec()
 	img = Image.create_from_data(width, height, false, Image.FORMAT_RGBA8, trailMapData)
+	print("getting new image: " + str(Time.get_ticks_msec() - start))
+	start = Time.get_ticks_msec()
 	_updateBuffers()
-
+	print("updating buffers: " + str(Time.get_ticks_msec() - start))
+	start = Time.get_ticks_msec()
 	tex.update(img)
+	print("updating texture: " + str(Time.get_ticks_msec() - start))
 	mat.set_shader_parameter("dataTex", tex)
 
 	#$SubViewportContainer/SubViewport/TextureRect.texture = tex
@@ -179,7 +194,7 @@ func _updateBuffers():
 	trailMapUniform.binding = 4
 	trailMapUniform.add_id(trailMapBuffer)
 	
-	var tempImg := Image.create_empty(width, height, false, Image.FORMAT_RF)
+	
 	rd.free_rid(trailMapOutBuffer)
 	trailMapOutBuffer = rd.texture_create(fmt, view, [tempImg.get_data()])
 	var trailMapOutUniform := RDUniform.new()
